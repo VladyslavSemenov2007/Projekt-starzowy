0c41d12cf9bbc0a4aa4e65977502bc3ad96c3746
VladyslavSemenov2007 <vladyslav.semenov@psr.gda.pl>
2026-04-29 15:59:04 +0200

    modified db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,14 +1,10 @@
 const sql = require('mssql');
 
-const config = {
-    connectionString: process.env.DB_CONNECTION_STRING
-};
-
 let pool = null;
 
 async function getPool() {
     if (!pool) {
-        pool = await sql.connect(config);
+        pool = await sql.connect(process.env.DB_CONNECTION_STRING);
     }
     return pool;
 }
