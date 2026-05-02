91d9bc4640f68661945fb3b6581df5a58d66cb64
VladyslavSemenov2007 <vladyslav.semenov@psr.gda.pl>
2026-05-02 15:45:03 +0200

    issue 13 hald done

--- backend/src/functions/record.js
+++ backend/src/functions/record.js
@@ -1,17 +1,22 @@
 const { app } = require('@azure/functions');
 const { getPool } = require('../db');
 
+const page = parseInt(req.query.get('page')) || 1;
+const limit = parseInt(req.query.get('limit')) || 10;
+const offset = (page - 1) * limit;
+
 app.http('records', {
     methods: ['GET'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
         try {
             const pool = await getPool();
-            const result = await pool.request().query('SELECT * from records');
+            const result = await pool.request().query('SELECT * from records OFFSET @offset ROWS FETCH NEXT @limit ROWS ONLY');
+            const total = await pool.request().query('SELECT count(*) from records');
 
             return {
                 status: 200,
-                jsonBody: { status: "ok", db: 'connected', message: result.recordset}
+                jsonBody: { data: result.recordset, total: total, page:page, limit:limit }
             };
         } catch (error) {
             return {
