207f2a09faf1c251f9488b3edebc2f9ecf2425b9
VladyslavSemenov2007 <vladyslav.semenov@psr.gda.pl>
2026-05-09 12:45:31 +0200

    console log 3

--- frontend/js/records.js
+++ frontend/js/records.js
@@ -16,7 +16,7 @@ async function loadRecords() {
       tbody.innerHTML = '<tr><td colspan="6">No records found</td></tr>';
       return;
     }
-    console.log("doszedlem do mapowania!!!")
+    console.log("doszedlem do mapowania!!!");
     tbody.innerHTML = records.data.map(record => `
       <tr>
         <td>${escapeHtml(record.name)}</td>
@@ -30,7 +30,7 @@ async function loadRecords() {
         </td>
       </tr>
     `).join('');
-    console.log("po mapowaniu")
+    console.log("po mapowaniu");
 
   } catch (error) {
     tbody.innerHTML = '<tr><td colspan="6">Error: ${error.message}</td></tr>';
